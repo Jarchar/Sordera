f2cefa32db58dbe9783a4fc22f623da0bda1039c
Bruo Estrada <brunoeb98@gmail.com>
2016-04-25 17:02:37 -0500

    Mueve videos con exito

--- app/controllers/PalabraController.java
+++ app/controllers/PalabraController.java
@@ -2,7 +2,12 @@ package controllers;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -69,6 +74,44 @@ public class PalabraController extends Controller {
            String fileName = picture.getFilename();
            String contentType = picture.getContentType();
            java.io.File file = picture.getFile();
+           Palabra pal= new Palabra();
+           Map<String,String[]> datos= body.asFormUrlEncoded();
+           pal.descripcion=datos.get("descripcion")[0];
+           pal.categoria=datos.get("categoria")[0];
+           pal.nombre=datos.get("nombre")[0];
+           pal.save();
+           InputStream inStream = null;
+       	OutputStream outStream = null;
+       		
+           	try{
+           		
+           	    File bfile =new File("/home/bruno/"+fileName);
+           		
+           	    inStream = new FileInputStream(file);
+           	    outStream = new FileOutputStream(bfile);
+               	
+           	    byte[] buffer = new byte[1024];
+           		
+           	    int length;
+           	    //copy the file content in bytes 
+           	    while ((length = inStream.read(buffer)) > 0){
+           	  
+           	    	outStream.write(buffer, 0, length);
+           	 
+           	    }
+           	 
+           	    inStream.close();
+           	    outStream.close();
+           	    
+           	    //delete the original file
+           	    file.delete();
+           	    
+           	    System.out.println("File is copied successful!");
+           	    
+           	}catch(IOException e){
+           	    e.printStackTrace();
+           	}
+           
            return GO_HOME;
        } else {
            flash("error", "Missing file");
